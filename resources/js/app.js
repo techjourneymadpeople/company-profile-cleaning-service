0d7a58c3d662844b47038965b7279b5305cac8c0
Farhan MS <farhanmohaemin14@gmail.com>
2026-08-26 13:25:32 +0700

    feat: install and configure frontend packages (Tailwind, CKEditor, Select2, jQuery, Flatpickr, SweetAlert2)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+import $ from 'jquery';
+import Swal from 'sweetalert2';
+import flatpickr from 'flatpickr';
+import select2 from 'select2';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+select2();
+
+window.$ = window.jQuery = $;
+window.Swal = Swal;
+window.flatpickr = flatpickr;
+window.ClassicEditor = ClassicEditor;
